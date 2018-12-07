9cd92b0988da253be7838622ba3077117b83b73e
Sridhar Cuddalore Parthasarathy <scp392@nyu.edu>
2018-12-07 11:56:05 -0600

    Add files via upload

---
+++ Skills/Intruder Alert/IntruderAlert.js
@@ -0,0 +1,64 @@
+misty.Debug("Intruder alert skill started");
+
+misty.Set("StartTime",(new Date()).toUTCString());
+misty.Set("Initiated",false);
+misty.Set("falseAlarm", 0);
+
+misty.StartFaceRecognition();
+
+registerFaceRec();
+
+function _FaceRec(data){
+    //misty.Debug("IN");
+    try{
+        if (data.PropertyTestResults[0].PropertyValue == "unknown person"){
+            var count = misty.Get("falseAlarm");
+            misty.Set("falseAlarm",count+1);
+            misty.Debug("FalseAlarm_Avoided");
+            if (misty.Get("falseAlarm")>3){        
+                misty.UnregisterEvent("FaceRec");
+                misty.Debug("Intruder Detected !!");
+                misty.SendExternalRequest("POST", "https://maker.ifttt.com","/trigger/blink_intruder/with/key/cfconLr0jZT4qT6mTRKImX",null,null,"{}");
+                misty.SendExternalRequest("POST", "https://maker.ifttt.com","/trigger/text_intruder/with/key/cfconLr0jZT4qT6mTRKImX",null,null,"{}");
+                misty.SendExternalRequest("POST", "https://maker.ifttt.com","/trigger/switch_intruder_on/with/key/cfconLr0jZT4qT6mTRKImX",null,null,"{}");
+                misty.Set("StartTime",(new Date()).toUTCString());
+                misty.Set("Initiated",true);
+                misty.ChangeDisplayImage("Disdainful.png");
+                misty.Set("falseAlarm", 0);
+            }
+        } else {
+            misty.Debug(data.PropertyTestResults[0].PropertyValue);
+            misty.Set("falseAlarm", 0);
+        }
+    
+    } catch (err) {
+        misty.Debug("Some Error");
+    }
+}
+
+function _SendExternalRequest(data_response) {
+    // Assign variables to grab the city name and current temperature
+        misty.Debug(JSON.stringify(data_response));
+}
+
+while (true) {
+    
+    if (misty.Get("Initiated")){
+        var timeElapsed = new Date() - new Date(misty.Get("StartTime"));
+        timeElapsed /= 1000;
+        var secondsElapsed = Math.round(timeElapsed);
+        misty.Debug(secondsElapsed);
+        if (secondsElapsed >= 14){
+            misty.SendExternalRequest("POST", "https://maker.ifttt.com","/trigger/switch_intruder_off/with/key/cfconLr0jZT4qT6mTRKImX",null,null,"{}");
+            registerFaceRec();
+            misty.Set("Initiated",false);
+            misty.ChangeDisplayImage("Homeostasis.png");
+        } 
+    } else {}
+    misty.Pause(50);
+}
+
+function registerFaceRec(){
+    misty.AddPropertyTest("FaceRec", "PersonName", "exists", "", "string");
+    misty.RegisterEvent("FaceRec", "ComputerVision", 100, true);
+}
